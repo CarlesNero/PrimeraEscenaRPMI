01216bcce5ea89dc129009bc140859d8c0af9d4a
Alfonso Gaitán <alfonsogaitanperez@gmail.com>
2024-01-23 09:47:44 +0100

    pito

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    public void LoadScene(string Escema2)
+    {
+        SceneManager.LoadScene(Escema2);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
